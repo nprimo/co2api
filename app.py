d6b118ddc0cdb9e7b08c18ee136cf6fe8cea1e4c
nprimo <primo.niccolo@gmail.com>
2022-05-24 22:57:26 +0100

    Update app.py to work with templates

--- app.py
+++ app.py
@@ -6,16 +6,18 @@
 #    By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/05/24 22:46:27 by nprimo            #+#    #+#              #
-#    Updated: 2022/05/24 22:46:53 by nprimo           ###   ########.fr        #
+#    Updated: 2022/05/24 22:54:55 by nprimo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from flask import Flask
+from flask import Flask, render_template
 from src.api_calls import get_carbon_intenisty, get_carbon_intensity_unit
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	text = "Electrical grid carbon intensity is currently {} {} in Portugal!".format(get_carbon_intenisty(), get_carbon_intensity_unit())
-	return text
+	return render_template(
+		'index.html',
+		carbon_intensity=get_carbon_intenisty(),
+		carbon_intensity_unit=get_carbon_intensity_unit())
