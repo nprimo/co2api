1421519dd3e36ac1d47a981ca62bceb4e1554513
nprimo <primo.niccolo@gmail.com>
2022-06-26 14:31:55 +0100

    Add get_co2_data function

--- src/api_calls.py
+++ src/api_calls.py
@@ -5,19 +5,26 @@ import json
 
 load_dotenv()
 
-url_base = "https://api.co2signal.com/v1/latest"
-url_country_code = "?countryCode="
-# To be able to choose eventually
-country_code = "PT" 
-url_header = {"auth-token": os.getenv("TOKEN")}
-response = requests.get(
-		url_base + url_country_code + country_code,
-		headers=url_header)
+def get_co2_data(country_code='PT'):
+  url_base = "https://api.co2signal.com/v1/latest"
+  url_country_code = "?countryCode=" 
+  url_header = {"auth-token": os.getenv("TOKEN")}
+  response = requests.get(
+		  url_base + url_country_code + country_code,
+		  headers=url_header)
+  return response
 
-def get_carbon_intenisty():
+
+def get_carbon_intenisty(response):
 	response_json = json.loads(response.text)
 	return response_json["data"]["carbonIntensity"]
 
-def get_carbon_intensity_unit():
+
+def get_carbon_intensity_unit(response):
 	response_json = json.loads(response.text)
 	return response_json["units"]["carbonIntensity"]
+
+
+def get_datetime(response):
+  response_json = json.load(response.text)
+  return response_json["data"]["datetime"]
